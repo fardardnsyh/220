4f656a8b26e451f51e138f43a94f57d0092eba69
Arduino <faridardiansyah061@gmail.com>
2024-09-09 14:36:44 +0700

    Update AbuotMe.tsx

--- src/components/contents/AbuotMe.tsx
+++ src/components/contents/AbuotMe.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import AnimationContainer from '../utils/AnimationContainer'
 
 const AbuotMe = () => {
     return (
-        <AnimationContainer customClassName='w-full py-12 lg:py-16'>
 
             <h2 className='mb-8 text-2xl font-bold tracking-tight text-center text-white lg:text-start'>
                 About me
@@ -13,7 +11,6 @@ const AbuotMe = () => {
                 I am currently on a thrilling journey pursuing my B.Tech degree in <strong>Mechanical Engineering</strong>. Beyond the world of gears and machines, I craft engaging and responsive web applications using <strong>React</strong> and delve into the realm of mobile app development with <strong>React Native</strong>. Apart from coding, I express my creative side by designing sleek interfaces using <strong>Figma</strong>. Let&apos;s build something amazing together! ✨
             </p>
 
-        </AnimationContainer>
     )
 }
 
